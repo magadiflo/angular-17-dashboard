ad8f66dc299283a58bb3af6f15d8a1611278bd9f
Martín <magadiflo@gmail.com>
2023-11-23 00:22:16 -0500

    Configuración de rutas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
-export class DashboardComponent {
+export default class DashboardComponent {
 
 }
