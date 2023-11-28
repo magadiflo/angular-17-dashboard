90e5a704af4a781b312b6c16fc091c0646814cb1
Martín <magadiflo@gmail.com>
2023-11-27 22:43:00 -0500

    Control Flow - @For

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -12,6 +12,8 @@ export default class ControlFlowComponent {
 
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'View', 'Svelte', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update(value => !value);
