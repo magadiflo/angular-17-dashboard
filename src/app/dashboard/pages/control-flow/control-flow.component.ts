183e064b43bfca58dafccf16eaf5d15ff280c566
Martín <magadiflo@gmail.com>
2023-11-27 22:58:42 -0500

    Required @Input

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 
+import { TitleComponent } from '@shared/title/title.component';
+
 type Grade = 'A' | 'B' | 'F'
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
+  template: `<h1 class="text-3xl mb-5">{{ title }} - {{ withShadow }}</h1>`,
   styleUrl: './title.component.scss'
 })
 export class TitleComponent {
+
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 
 }
