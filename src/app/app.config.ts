11d73254ac8e0f0b1dd2b84f4e951e209c126872
Martín <magadiflo@gmail.com>
2023-11-29 23:44:09 -0500

    ViewTransitions API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,29 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 
+/***
+ * withViewTransitions(), tan solo agregando esta función, veremos que al cambiar de páginas
+ * en nuestra aplicación veremos una ligera transición, agradable a la vista.
+ *
+ * Luego, podemos agregar algunos atributos dentro de la función:
+ * skipInitialTransition: true, que la primera vez que cargue la aplicación se salte la animación de transición.
+ * onViewTransitionCreated(trasitionInfo) {
+ *    console.log(trasitionInfo); //Nos da cierta información sobre la transición.
+ * },
+ */
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(APP_ROUTES),
+    provideRouter(
+      APP_ROUTES,
+      withViewTransitions({
+        skipInitialTransition: true,
+        // onViewTransitionCreated(trasitionInfo) {
+        //   console.log(trasitionInfo);
+        // },
+      }),
+    ),
   ]
 };
---
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { TitleComponent } from '@shared/title/title.component';
+
+/**
+ * srcset, permite realizar ciertas optimizaciones de código.
+ */
+@Component({
+  standalone: true,
+  imports: [TitleComponent],
+  template: `
+  <app-title title="View Transition 1"/>
+  <section class="flex justify-start">
+    <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300">
+    <div class="bg-blue-500 w-56 h-56"></div>
+  </section>
+  `,
+})
+export default class ViewTransitionComponent {
+
+}
