2a461f3a6b881b4268e681fdbdf082d7179bf37f
Martín <magadiflo@gmail.com>
2023-11-29 23:52:49 -0500

    Hero Animation - View Transitions

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -11,8 +11,9 @@ import { TitleComponent } from '@shared/title/title.component';
   template: `
   <app-title title="View Transition 1"/>
   <section class="flex justify-start">
-    <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300">
-    <div class="bg-blue-500 w-56 h-56"></div>
+    <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"
+    style="view-transition-name: hero1">
+    <div class=" fixed bottom-16 right-10 bg-blue-500 w-56 h-56" style="view-transition-name: hero2"></div>
   </section>
   `,
 })
